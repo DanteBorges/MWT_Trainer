0ae58f26ef41faf8ce3c4aa14dd23ccb28ae9edf
DanteBorges <danielnorges@gmail.com>
2021-10-28 18:46:35 -0300

    .container_list_items fixed, slide scroll button added

--- JS/script.js
+++ JS/script.js
@@ -11,7 +11,32 @@ $(document).ready(function() {
       autoplaySpeed: 2000,
       nextArrow: $(".next"),
       prevArrow: $(".prev"),
-   
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+       
+      ]
     });
   });
   
