f433b73e87240b46c1609bd362499be556a85f46
DanteBorges <danielnorges@gmail.com>
2021-10-29 16:41:25 -0300

    slide added to testimonials

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,45 @@
-$(document).ready(function() {
-    $(".menu-toggle").on("click", function() {
-      $(".nav").toggleClass("showing");
-      $(".nav ul").toggleClass("showing");
-    });
-  
-    $(".post-wrapper").slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      nextArrow: $(".next"),
-      prevArrow: $(".prev"),
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
+$(document).ready(function () {
+  $(".menu-toggle").on("click", function () {
+    $(".nav").toggleClass("showing");
+    $(".nav ul").toggleClass("showing");
+  });
+
+  $(".post-wrapper").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
         },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
         },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-       
-      ]
-    });
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
   
-  
+});
+$(".testimonials_carousel_container_items").slick({
+  dots:true,
+  infinite: true
+});
